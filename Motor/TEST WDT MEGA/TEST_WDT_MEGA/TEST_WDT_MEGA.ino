ace8fe4e8a6f61e2196f6274e182a54d4ca9b4f9
KST\rahul <rahulnrathodkcl@gmail.com>
2017-04-21 19:39:12 +0530

    for testing...
    no connection with the main program..

--- Motor/TEST WDT MEGA/TEST_WDT_MEGA/TEST_WDT_MEGA.ino
+++ Motor/TEST WDT MEGA/TEST_WDT_MEGA/TEST_WDT_MEGA.ino
@@ -1,4 +1,17 @@
 #include <avr/wdt.h>
+#define WATCHDOG_OFF    (0)
+#define WATCHDOG_16MS   (_BV(WDE))
+#define WATCHDOG_32MS   (_BV(WDP0) | _BV(WDE))
+#define WATCHDOG_64MS   (_BV(WDP1) | _BV(WDE))
+#define WATCHDOG_125MS  (_BV(WDP1) | _BV(WDP0) | _BV(WDE))
+#define WATCHDOG_250MS  (_BV(WDP2) | _BV(WDE))
+#define WATCHDOG_500MS  (_BV(WDP2) | _BV(WDP0) | _BV(WDE))
+#define WATCHDOG_1S     (_BV(WDP2) | _BV(WDP1) | _BV(WDE))
+#define WATCHDOG_2S     (_BV(WDP2) | _BV(WDP1) | _BV(WDP0) | _BV(WDE))
+#ifndef __AVR_ATmega8__
+#define WATCHDOG_4S     (_BV(WDP3) | _BV(WDE))
+#define WATCHDOG_8S     (_BV(WDP3) | _BV(WDP0) | _BV(WDE))
+#endif
 
 
 void wdt_init(void) __attribute__((naked)) __attribute__((section(".init3")));
@@ -17,6 +30,18 @@ void setup()
   Serial.begin(19200);
 }
 
+
+void watchdogReset() {
+  __asm__ __volatile__ (
+    "wdr\n"
+  );
+}
+
+void watchdogConfig(uint8_t x) {
+  WDTCSR = _BV(WDCE) | _BV(WDE);
+  WDTCSR = x;
+}
+
 void loop()
 {
   delay(500);
@@ -25,16 +50,31 @@ void loop()
   if(Serial.available())
   {
     String str=Serial.readStringUntil('\n');
-    if(str=="RESET\r")
+    if(str=="WDT\r")
     {
+      noInterrupts();
       do                          
       {
+        noInterrupts();
           wdt_enable(WDTO_15MS);  
           for(;;)                 
           {                       
           }                       
       } while(0);
 
+    }
+    else
+    {
+    if(str=="WDTE\r")
+    { 
+      noInterrupts();
+      watchdogReset();
+      watchdogConfig(WATCHDOG_16MS);
+          for(;;)
+          {                       
+          }                       
+    }
+      
     }
   }
 }
